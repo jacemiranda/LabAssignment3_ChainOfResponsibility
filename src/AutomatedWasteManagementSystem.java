200e4a73c629055c4c9b04420b33ed943466fd6b
Jermaine Carl Miranda <jermaine.carlm@gmail.com>
2026-02-28 22:37:59 +0800

    Update AutomatedWasteManagementSystem.java

--- src/AutomatedWasteManagementSystem.java
+++ src/AutomatedWasteManagementSystem.java
@@ -1,6 +1,6 @@
 public class AutomatedWasteManagementSystem {
     public static void main(String[] args) {
-        // Initialize the chain of collectors
+        
         WasteCollector organic = new OrganicCollector();
         WasteCollector recyclable = new RecyclableCollector();
         WasteCollector hazardous = new HazardousCollector();
@@ -8,13 +8,11 @@ public class AutomatedWasteManagementSystem {
         organic.setNextCollector(recyclable);
         recyclable.setNextCollector(hazardous);
 
-        // Create different waste containers
         WasteContainer container1 = new WasteContainer("organic", 50);
         WasteContainer container2 = new WasteContainer("recyclable", 30);
         WasteContainer container3 = new WasteContainer("hazardous", 10);
-        WasteContainer container4 = new WasteContainer("electronic", 5); // Unsupported type
+        WasteContainer container4 = new WasteContainer("electronic", 5);
 
-        // Trigger disposal process [cite: 13, 14]
         System.out.println("--- Starting Waste Collection Process ---");
         organic.collect(container1);
         organic.collect(container2);
